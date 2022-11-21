002b4a31a351d287e3d61e8fe7ddaa64ddf60ec2
Rrezon Istrefaj <xoni-pr@hotmail.com>
2022-11-21 20:23:56 +0100

    mailto v3

--- src/components/card.js
+++ src/components/card.js
@@ -1,166 +1,160 @@
-
 import styled from "styled-components";
-import React, { useState ,useEffect} from 'react'
-import ButtonMailto from "./mailto";
+import React, { useState ,useLayoutEffect} from 'react'
+// import ButtonMailto from "./mailto";
 
 
  
 function Card() {
-  const initialValues = { username: "", email: "", feedback: "" };
-  const [formValues, setFormValues] = useState(initialValues);
-  const [formErrors, setFormErrors] = useState({});
-  const [isSubmit, setIsSubmit] = useState(false);
-  const href = `${ButtonMailto}?${url.toString()}`;
+  const initialValues = { username: "", email: "", feedback: "" };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormValues({ ...formValues, [name]: value });
-  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setFormErrors(validate(formValues));
-    setIsSubmit(true);
-  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  };
 
-  useEffect(() => {
-    console.log(formErrors);
-    if (Object.keys(formErrors).length === 0 && isSubmit) {
-      console.log(formValues);
-    }
-  }, [formErrors]);
-  const validate = (values) => {
-    const errors = {};
-    if (!values.username) {
-      errors.username = "Full Name is required!";
-    }
-    if (!values.email) {
-      errors.email = "Email is required!";
-    } 
-    if (!values.feedback) {
-      errors.feedback = "Feedback is required!";
-    } else if (values.feedback.length < 10) {
-      errors.feedback = "Feedback must be more than 10 characters";
-    }
-    return errors;
-  };
- 
-  
+  useLayoutEffect(() => {
+    console.log(formErrors);
+    setFormErrors(validate(formValues));
+  }, [formValues]);
+  const validate = (values) => {
+    const errors = {};
+    if (!values.username) {
+      errors.username = "Full Name is required!";
+    }
+    if (!values.email) {
+      errors.email = "Email is required!";
+    } 
+    if (!values.feedback) {
+      errors.feedback = "Feedback is required!";
+    } else if (values.feedback.length < 10) {
+      errors.feedback = "Feedback must be more than 10 characters";
+    }
+    return errors;
+  };
 
-  return (
-    
-    <CardStyled>
-      
-              <div className="container">
-              {/* <pre>{JSON.stringify(formValues,undefined)}</pre> */}
-    <form onSubmit={handleSubmit}>
+const url = new URL();
+  url.append('subject', `Rrezons Feedback Form from ${formValues.username}`);
+  url.append('body', `${formValues.feedback} \n from ${formValues.email}`);
 
-      <div className="input-group">
-        <label>Full Name</label>
-        <input type="text" name="username" placeholder="Enter your name"id="username" value={formValues.username} onChange={handleChange}>
-        </input>
-      </div>
-        <p>{formErrors.username}</p>
-      <div className="input-group">
-        <label>Email</label>
-        <input type="email" name="email" placeholder="Enter Email"id="email" value={formValues.email} onChange={handleChange}
+const mailTo = `mailto:shselaci3@gmail.com`;
+
+const href = `${mailTo}?${url.toString()}`;
+  return (
+    <CardStyled>
+              <div className="container">
+              {/* <pre>{JSON.stringify(formValues,undefined)}</pre> */}
+    <form onSubmit={handleSubmit}>
+
+      <div className="input-group">
+        <label>Full Name</label>
+        <input type="text" name="username" placeholder="Enter your name" value={formValues.username} onChange={handleChange}>
+        </input>
+      </div>
+        <p>{formErrors.username}</p>
+      <div className="input-group">
+        <label>Email</label>
+        <input type="email" name="email" placeholder="Enter Email" value={formValues.email} onChange={handleChange}
 ></input>
 
-      </div>   
-      <p>{formErrors.email}</p>
-      <div className="input-group">
-        <label>Your Message</label>
-        <textarea name="feedback"
-        id="textarea"
-        rows="5" 
-        placeholder="Enter your message"  
-        value={formValues.feedback}
-        onChange={handleChange}>
-            
-        </textarea>
-      </div>
-      <p>{formErrors.feedback}</p>
-     
+      </div>   
+      <p>{formErrors.email}</p>
+      <div className="input-group">
+        <label>Your Message</label>
+        <textarea name="feedback"
+        rows="5" 
+        placeholder="Enter your message"  
+        value={formValues.feedback}
+        onChange={handleChange}>
+            
+        </textarea>
+      </div>
+      <p>{formErrors.feedback}</p>
 
-      <ButtonMailto >
-        <a href={href} disabled={Object.values(validate(formValues).length)}>Submit</a>
-      </ButtonMailto>
+      <a href={href} disabled={Object.values(validate(formValues)).length}>Submit</a>   
 
-  </form>
-  </div>
-    </CardStyled>
-  )
+    
+   
+  </form>
+  </div>
+    </CardStyled>
+  )
 }
 
-
-  
 const CardStyled = styled.div`
 p{
-    color: red;
-    display: flex;
-    align-items: right;
+    color: red;
+    display: flex;
+    align-items: right;
 }
 .container{
-    width: 100%;
-    height: 100vh;
-    background: #141a34;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+    width: 100%;
+    height: 100vh;
+    background: #141a34;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 .container form{
-    width: 90%;
-    max-width: 500px;
-    padding: 50px 30px 20px;
-    background: #fff;
-    border-radius: 4px;
-    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.5);
-    position: relative;  
+    width: 90%;
+    max-width: 500px;
+    padding: 50px 30px 20px;
+    background: #fff;
+    border-radius: 4px;
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.5);
+    position: relative;  
 }
 .input-group{
-    width: 100%;
-    display: flex;
-    align-items: center;
-    margin: 10px 0;
-    position: relative;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    margin: 10px 0;
+    position: relative;
 }
 .input-group label{
-    flex-basis: 28%;
+    flex-basis: 28%;
 }
 .input-group input, .input-group textarea{
-    flex-basis: 68%;
-    background: transparent;
-    border: 0;
-    outline: 0;
-    padding: 10px 0;
-    border-bottom: 1px solid #999;
-    color: #333;
-    font-size: 16px;
+    flex-basis: 68%;
+    background: transparent;
+    border: 0;
+    outline: 0;
+    padding: 10px 0;
+    border-bottom: 1px solid #999;
+    color: #333;
+    font-size: 16px;
 }
 ::placeholder{
-    font-size: 14px;
+    font-size: 14px;
 }
 
-button{
-    background: #141a34;
-    color: #fff;
-    border-radius: 4px;
-    border: 1px solid rgba(255, 255, 255, 0.7);
-    padding: 10px 40px;
-    outline: 0;
-    cursor: pointer;
-    display: block;
-    margin: 30px auto 10px;
+form button{
+    background: #141a34;
+    color: #fff;
+    border-radius: 4px;
+    border: 1px solid rgba(255, 255, 255, 0.7);
+    padding: 10px 40px;
+    outline: 0;
+    cursor: pointer;
+    display: block;
+    margin: 30px auto 10px;
 }
 .input-group span{
-    position: absolute;
-    bottom: 12px;
-    right: 17px;
-    font-size:14px;
-    color: red;
+    position: absolute;
+    bottom: 12px;
+    right: 17px;
+    font-size:14px;
+    color: red;
 }
 
 `;
 
 export default Card
-
